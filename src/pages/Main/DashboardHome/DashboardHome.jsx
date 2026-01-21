faa8d4df292efe39735ef9c395757ebfcbcfb955
shejanNizam <nizam.shejan11@gmail.com>
2026-01-21 18:31:16 +0600

    dashboard design complete

--- src/pages/Main/DashboardHome/DashboardHome.jsx
+++ src/pages/Main/DashboardHome/DashboardHome.jsx
@@ -1,53 +1,48 @@
-import dayjs from "dayjs";
-import { useState } from "react";
 import { FaUsers } from "react-icons/fa6";
-import { MdOutlineSportsGymnastics } from "react-icons/md";
-import { useGetAllStatsQuery } from "../../../redux/features/dashboardHome/dashboardHomeApi";
 import { StatCard } from "../../../utils/utils";
-import DashboardChart from "./DashboardChart";
 
 export default function DashboardHome() {
-  const [year, setYear] = useState(dayjs().year());
-  const { data } = useGetAllStatsQuery(year);
+  // const [year, setYear] = useState(dayjs().year());
+  // const { data } = useGetAllStatsQuery(year);
   // console.log(data?.data);
 
-  const totalParents = data?.data?.totalParents || "N/A";
-  const totalTutors = data?.data?.totalTutors || "N/A";
-  const totalSessions = data?.data?.totalSessions || "N/A";
-  const monthlySessions = data?.data?.monthlySessions || [];
+  // const totalParents = data?.data?.totalParents || "N/A";
+  // const totalTutors = data?.data?.totalTutors || "N/A";
+  // const totalSessions = data?.data?.totalSessions || "N/A";
+  // const monthlySessions = data?.data?.monthlySessions || [];
+
+  const totalParents = "1320";
+  const totalTutors = "8";
 
   return (
     <div className="space-y-6 p-2 md:p-4">
+      <div className="border border-gray-200 rounded-xl shadow-sm p-4">
+        <h3>Hi, Good Morning</h3>
+        <h2 className="text-2xl font-semibold">Mini Roy</h2>
+      </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <StatCard
-          title="Total Parents"
+          title="Total Users"
           value={totalParents}
           icon={FaUsers}
           colorClass="bg-blue-100 text-blue-600"
         />
 
         <StatCard
-          title="Total Tutors"
+          title="Todays New User's"
           value={totalTutors}
           icon={FaUsers}
           colorClass="bg-green-100 text-green-600"
         />
-
-        <StatCard
-          title="Total Sessions"
-          value={totalSessions}
-          icon={MdOutlineSportsGymnastics}
-          colorClass="bg-yellow-100 text-yellow-600"
-        />
       </div>
 
-      <div>
+      {/* <div>
         <DashboardChart
           monthlySessions={monthlySessions}
           year={year}
           setYear={setYear}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
